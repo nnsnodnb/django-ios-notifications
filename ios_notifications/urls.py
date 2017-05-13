cb342aa92624aa16ac7778c2a758e15915d7fd72
nnsnodnb <ahr63_gej@me.com>
2017-05-13 14:15:26 +0900

    Modify django project urls

--- ios_notifications/urls.py
+++ ios_notifications/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('notification.urls'), namespace='notification'),
+    # url(r'', include('notification.urls'), namespace='notification'),
 ]
