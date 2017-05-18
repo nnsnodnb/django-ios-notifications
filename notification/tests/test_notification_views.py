f29ea215ab50a6d1ba0957f3f89241a24f6ee597
nnsnodnb <ahr63_gej@me.com>
2017-05-19 00:53:44 +0900

    Support python 3.5

--- notification/tests/test_notification_views.py
+++ notification/tests/test_notification_views.py
@@ -18,29 +18,36 @@ class NotificationViewsTestCase(TestCase):
         request.content_type = 'application/json'
         response = device_token_receive(request)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), {'result': 'success'})
+        self.assertEqual(json.loads(response.content.decode('utf-8')),
+                         {'result': 'success'})
 
     def test_device_token_receive_with_only_device_token(self):
         parameter = {'device_token': '8a0d7cba3ffad34bd3dcb37728080a95d6ee78a83a68ead033614acbab9b7e76'}
         request = self.factory.put('/receive/',
                                    json.dumps(parameter))
+        request.content_type = 'application/json'
         response = device_token_receive(request)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.content), {'error': 'Bad Request'})
+
+        self.assertEqual(json.loads(response.content.decode('utf-8')),
+                         {'error': 'Bad Request'})
 
     def test_device_token_receive_with_only_uuid(self):
         parameter = {'uuid': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'}
         request = self.factory.put('/receive/',
                                    json.dumps(parameter))
+        request.content_type = 'application/json'
         response = device_token_receive(request)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.content), {'error': 'Bad Request'})
+        self.assertEqual(json.loads(response.content.decode('utf-8')),
+                         {'error': 'Bad Request'})
 
     def test_device_token_receive_without_parameter(self):
         request = self.factory.put('/receive/')
         response = device_token_receive(request)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.content), {'error': 'Bad Request'})
+        self.assertEqual(json.loads(response.content.decode('utf-8')),
+                         {'error': 'Bad Request'})
 
     def test_device_token_receive_method_get(self):
         request = self.factory.get('/receive/')
