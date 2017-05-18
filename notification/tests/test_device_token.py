9de3c5dfbddec4ef03e02f7eb5b6bb0f4b2858c9
nnsnodnb <ahr63_gej@me.com>
2017-05-18 18:23:39 +0900

    Add DeviceToken models' test case

--- notification/tests/test_device_token.py
+++ notification/tests/test_device_token.py
@@ -1,7 +1,50 @@
-from ..views import device_token_receive
 from ..models import DeviceToken
+from django.contrib.auth.models import User
 from unittest import TestCase
 
 
 class DeviceTokenTestCase(TestCase):
-    pass
+
+    def tearDown(self):
+        for device_token in DeviceToken.objects.all():
+            device_token.delete()
+        for user in User.objects.all():
+            user.delete()
+
+    def test_no_device_token(self):
+        device_tokens = DeviceToken.objects.all()
+        self.assertCountEqual(device_tokens, [])
+
+    def test_one_device_token_without_user(self):
+        device_token = DeviceToken()
+        device_token.device_token = '8a0d7cba3ffad34bd3dcb37728080a95d6ee78a83a68ead033614acbab9b7e76'
+        device_token.uuid = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+        device_token.save()
+
+        get_device_token = DeviceToken.objects.filter().first()
+        self.assertEqual(device_token.device_token, get_device_token.device_token)
+        self.assertEqual(device_token.uuid, get_device_token.uuid)
+        self.assertEqual(device_token.user, get_device_token.user)
+        self.assertIsNone(get_device_token.user)
+
+    def test_one_device_token_with_user(self):
+        device_token = DeviceToken()
+        device_token.device_token = '8a0d7cba3ffad34bd3dcb37728080a95d6ee78a83a68ead033614acbab9b7e76'
+        device_token.uuid = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+
+        user = User.objects.create_user(username='test_case',
+                                        email='test_case@localhost',
+                                        password='test_case_test_case')
+        user.save()
+
+        device_token.user = user
+        device_token.save()
+
+        get_device_token = DeviceToken.objects.filter().first()
+        self.assertEqual(device_token.device_token, get_device_token.device_token)
+        self.assertEqual(device_token.uuid, get_device_token.uuid)
+        self.assertEqual(device_token.user, get_device_token.user)
+        self.assertIsNotNone(get_device_token.user)
+
+        get_user = User.objects.filter().first()
+        self.assertEqual(user, get_user)
