cdf5dd2207445c230c0a01947cceefaa86b0735e
nnsnodnb <ahr63_gej@me.com>
2017-05-22 22:09:12 +0900

    [WIP] Add send_notification_with_device_token test #6

--- notification/tests/test_notification_views.py
+++ notification/tests/test_notification_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test.client import RequestFactory
 from unittest import TestCase
 from ..models import DeviceToken
@@ -7,19 +8,14 @@ import json
 import os
 
 
-class NotificationViewsTestCase(TestCase):
-
+class NotificationViewDeviceTokenReceiveTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.device_token = '8a0d7cba3ffad34bd3dcb37728080a95d6ee78a83a68ead033614acbab9b7e76'
         self.uuid = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
-        self.cert_file_path = os.path.dirname(os.path.abspath(__file__)) + '/cert.pem'
 
     def tearDown(self):
         DeviceToken.objects.all().delete()
-        if not os.path.isfile(self.cert_file_path):
-            return
-        os.remove(self.cert_file_path)
 
     def tearDown(self):
         DeviceToken.objects.all().delete()
@@ -97,14 +93,41 @@ class NotificationViewsTestCase(TestCase):
         response = device_token_receive(request)
         self.assertEqual(response.status_code, 405)
 
-    def test_send_notification_with_device_token_target_develop_is_device_token_is_not_message(self):
+
+class NotificationViewsSendNotificationWithDeviceTokenTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.device_token = '8a0d7cba3ffad34bd3dcb37728080a95d6ee78a83a68ead033614acbab9b7e76'
+        self.uuid = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+        self.cert_file_path = os.path.dirname(os.path.abspath(__file__)) + '/cert.pem'
+        self.super_user = User.objects.create_superuser(username='super_user',
+                                                        password='test_case_for_super_user',
+                                                        email='super_user@localhost')
+        self.super_user.save()
+        self.general_user = User.objects.create_user(username='general_user',
+                                                     password='test_case_for_general_user')
+        self.general_user.save()
+
+    def tearDown(self):
+        self.super_user.delete()
+        self.general_user.delete()
+        if not os.path.isfile(self.cert_file_path):
+            return
+        os.remove(self.cert_file_path)
+
+    def test_target_develop_is_device_token_is_not_message_is_super_user(self):
+        # Target: Development
+        # Device token is here.
+        # Message is not here.
+        # Request by super user
         device_token = DeviceToken(device_token=self.device_token,
                                    uuid=self.uuid)
         device_token.save()
         f = open(self.cert_file_path, 'w')
         f.write('test_case_test_case_test_case')
         f.close()
-
         request = self.factory.get('/send/0/' + self.device_token)
+        request.user = self.super_user
         response = send_notification_with_device_token(request)
         self.assertEqual(response.status_code, 200)
