299a85e3d607a35c1575ad625ed9fe71f4c3c4e4
nnsnodnb <ahr63_gej@me.com>
2017-06-03 23:43:40 +0900

    [WIP] Modify multipush commands' test case #11

--- notification/tests/test_notification_command_multipush.py
+++ notification/tests/test_notification_command_multipush.py
@@ -5,12 +5,27 @@ from django.core.management import call_command
 class ManagementCommandsMultiPushTest(TestCase):
 
     def setUp(self):
-        pass
+        self.options = {'verbosity': 1,
+                        'settings': None,
+                        'pythonpath': None,
+                        'traceback': False,
+                        'no_color': False,
+                        'sandbox': False,
+                        'device_tokens': None,
+                        'all': False,
+                        'title': None,
+                        'subtitle': None,
+                        'body': None,
+                        'sound': 'default',
+                        'badge': 1,
+                        'content_available': False,
+                        'mutable_content': False,
+                        'extra': None}
 
     def tearDown(self):
         pass
 
-    def test_hoge(self):
+    def test_without_device_tokens(self):
         args = []
-        opts = {}
-        call_command('multipush', *args, **opts)
+        
+        call_command('multipush', *args, **self.options)
