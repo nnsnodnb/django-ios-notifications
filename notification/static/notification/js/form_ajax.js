b67d098f4a0db0992ff4c7bf8032d361236c1c4f
nnsnodnb <ahr63_gej@me.com>
2017-06-12 01:33:56 +0900

    Add session for send_form #19

--- notification/static/notification/js/form_ajax.js
+++ notification/static/notification/js/form_ajax.js
@@ -1,9 +1,60 @@
 $(function() {
     $('#id_target').attr('onClick', 'change_target()');
     $('#id_target').attr('name', 'target');
-})
+
+    if (sessionStorage.getItem(['send_title'])) {
+        $('#id_title').prop('value', sessionStorage.getItem(['send_title']));
+        sessionStorage.removeItem(['send_title']);
+    }
+    if (sessionStorage.getItem(['send_subtitle'])) {
+        $('#id_subtitle').prop('value', sessionStorage.getItem(['send_subtitle']));
+        sessionStorage.removeItem(['send_subtitle']);
+    }
+    if (sessionStorage.getItem(['send_body'])) {
+        $('#id_body').prop('value', sessionStorage.getItem(['send_body']));
+        sessionStorage.removeItem(['send_body']);
+    }
+    if (sessionStorage.getItem(['send_sound'])) {
+        $('#id_sound').prop('value', sessionStorage.getItem(['send_sound']));
+        sessionStorage.removeItem(['send_sound']);
+    }
+    if (sessionStorage.getItem(['send_badge'])) {
+        $('#id_badge').prop('value', sessionStorage.getItem(['send_badge']));
+        sessionStorage.removeItem(['send_badge']);
+    }
+    if (sessionStorage.getItem(['send_extra'])) {
+        $('#id_extra').prop('value', sessionStorage.getItem(['send_extra']));
+        sessionStorage.removeItem(['send_extra']);
+    }
+});
 
 function change_target() {
+    title = $('#id_title').val();
+    subtitle = $('#id_subtitle').val();
+    body = $('#id_body').val();
+    sound = $('#id_sound').val();
+    badge = $('#id_badge').val();
+    extra = $('#id_extra').val();
+
+    if (title) {
+        sessionStorage.setItem(['send_title'], [title]);
+    }
+    if (subtitle) {
+        sessionStorage.setItem(['send_subtitle'], [subtitle]);
+    }
+    if (body) {
+        sessionStorage.setItem(['send_body'], [body]);
+    }
+    if (sound) {
+        sessionStorage.setItem(['send_sound'], [sound]);
+    }
+    if (badge) {
+        sessionStorage.setItem(['send_badge'], [badge]);
+    }
+    if (extra) {
+        sessionStorage.setItem(['send_extra'], [extra]);
+    }
+
     targets = $('input[name="target"]')
     if (targets[0].checked == true) {
         location.href = location.pathname + '?target=' + targets[0].value;
