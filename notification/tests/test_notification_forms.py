a7eccf7ca58cdd74d3a8a825f92bd6294d510562
nnsnodnb <ahr63_gej@me.com>
2017-06-19 23:23:16 +0900

    Add NotificationSendForm's test case #19

--- notification/tests/test_notification_forms.py
+++ notification/tests/test_notification_forms.py
@@ -1,6 +1,6 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 from unittest import TestCase
-from ..forms import CertFileUploadForm
+from ..forms import CertFileUploadForm, NotificationSendForm
 
 import os
 
@@ -25,3 +25,30 @@ class CertFileUploadFormTest(TestCase):
             file_parameter = {'cert_file': SimpleUploadedFile(f.name, f.read())}
             form = CertFileUploadForm(files=file_parameter)
             self.assertFalse(form.is_valid())
+
+
+class NotificationSendFormTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_is_valid_notification_send_form(self):
+        post_parameter = {'target': 0,
+                          'title': 'test title',
+                          'subtitle': 'test subtitle',
+                          'body': 'test body',
+                          'sound': 'default',
+                          'badge': 1,
+                          'content_available': False,
+                          'mutable_content': False,
+                          'extra': ''}
+        form = NotificationSendForm(post_parameter)
+        self.assertTrue(form.is_valid())
+
+    def test_is_invalid(self):
+        post_parameter = {}
+        form = NotificationSendForm(post_parameter)
+        self.assertFalse(form.is_valid())
