f992f80510405eae2c709f5c64cdf552aa606ce3
nnsnodnb <ahr63_gej@me.com>
2018-02-18 04:55:09 +0900

    Update models.py

--- notification/models.py
+++ notification/models.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 
@@ -7,7 +7,14 @@ class DeviceToken(models.Model):
     device_token = models.CharField(blank=False, default='', max_length=255)
     uuid = models.CharField(blank=False, default='', max_length=255)
     use_sandbox = models.BooleanField(blank=False, default=True)
-    user = models.ForeignKey(User, null=True, blank=True, default=None, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, default=None, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Device Tokens'
+        verbose_name_plural = 'Device Token'
+
+    def __str__(self):
+        return device_token
 
 
 class CertFile(models.Model):
@@ -15,3 +22,10 @@ class CertFile(models.Model):
     target_mode = models.IntegerField(blank=False, default=0)
     is_use = models.BooleanField(blank=False, default=True)
     date = models.DateTimeField(blank=False, default=now)
+
+    class Meta:
+        verbose_name = 'Certificate files'
+        verbose_name_plural = 'Certificate file'
+
+    def __str__(self):
+        return filename
