af2ab95fc978adbe8b64eaa62a5f4c1672989a73
nnsnodnb <ahr63_gej@me.com>
2018-03-28 22:19:32 +0900

    Update CertFileAdmin's fields

--- notification/admin.py
+++ notification/admin.py
@@ -6,11 +6,14 @@ class DeviceTokenAdmin(admin.ModelAdmin):
     list_display = ('id', 'device_token', 'uuid', 'user')
     list_display_links = ('id', 'device_token', 'uuid', 'user')
 
+
 admin.site.register(DeviceToken, DeviceTokenAdmin)
 
 
 class CertFileAdmin(admin.ModelAdmin):
-	list_display = ('id', 'filename', 'target_mode', 'is_use', 'date')
-	list_display_links = ('id', 'filename', 'target_mode')
+    readonly_fields = ('filename', 'target_mode', 'is_use', 'date')
+    list_display = ('id', 'filename', 'target_mode', 'is_use', 'date')
+    list_display_links = ('id', 'filename', 'target_mode')
+
 
 admin.site.register(CertFile, CertFileAdmin)
